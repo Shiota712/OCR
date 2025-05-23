14b01e5a6306e2741471079336eb120717fab2b0
Shiota712 <r122320@g.kobe-kosen.ac.jp>
2025-05-24 05:28:57 +0900

    vev.4 log追加

--- env/tests/auth/routes.py
+++ env/tests/auth/routes.py
@@ -1,23 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 import sqlite3
-import os
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-DB_PATH = 'users.db'
+DB_PATH = 'env/tests/db/users.db'
 
-# DB初期化（users.dbが存在しなければ作成する）
-def init_db():
-    if not os.path.exists(DB_PATH):
-        conn = sqlite3.connect(DB_PATH)
-        c = conn.cursor()
-        c.execute('CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT)')
-        conn.commit()
-        conn.close()
-
-init_db()
-
-
+# ログイン機能　ユーザーの入力情報を取得して保存
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -26,23 +14,25 @@ def login():
 
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
-        c.execute('SELECT password FROM users WHERE username = ?', (username,)) #SQLに保存
+        c.execute('SELECT password FROM users WHERE username = ?', (username,))     # ユーザーのpasswordを検索
         row = c.fetchone()
 
         if row:
-            # ログイン処理
+            # ログイン
             if row[0] == password:
                 session['username'] = username
                 return redirect(url_for('auth.index'))
             else:
                 flash('パスワードが違います')
         else:
-            # 新規登録処理
-            c.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+            # 新規登録
+            c.execute('INSERT OR REPLACE INTO users (username, password) VALUES (?, ?)', (username, password))
             conn.commit()
             session['username'] = username
+
             flash('新しいユーザーを登録しました')
             return redirect(url_for('auth.index'))
+        
         conn.close()
 
     return render_template('login.html')
@@ -52,3 +42,4 @@ def index():
     if 'username' in session:
         return render_template('index.html', username=session['username'])
     return redirect(url_for('auth.login'))
+
